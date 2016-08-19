6c3b2ad132f60a07d9019975c62a8897f529efae
Josh Day <emailjoshday@gmail.com>
2016-08-19 08:12:09 -0400

    basic structure

--- src/Penalties.jl
+++ src/Penalties.jl
@@ -1,5 +1,52 @@
 module Penalties
 
-# package code goes here
+using Reexport
+importall LearnBase
+@reexport using LearnBase
 
-end # module
+export
+    L1Penalty
+
+#------------------------------------------------------------------# abstract methods
+function value{T <: Number}(p::Penalty, λ::T, x::AbstractArray{T})
+    result = zero(T)
+    for xi in x
+        result += value(p, λ, xi)
+    end
+    result
+end
+
+function grad!{T<:Number}(dest::AbstractArray{T}, p::Penalty, λ::T, x::AbstractArray{T})
+    @assert size(dest) == size(x)
+    for i in eachindex(dest)
+        @inbounds dest[i] = deriv(p, λ, x[i])
+    end
+    dest
+end
+function grad{T<:Number}(p::Penalty, λ::T, x::AbstractArray{T})
+    dest = zeros(x)
+    grad!(dest, p, λ, x)
+end
+
+function prox!{T<:Number}(p::Penalty, λ::T, x::AbstractArray{T})
+    for i in eachindex(x)
+        @inbounds x[i] = prox(p, λ, x[i])
+    end
+    x
+end
+
+
+soft_thresh{T<:Number}(x::T, λ::T) = sign(x) * max(zero(x), abs(x) - λ)
+function soft_thresh!{T<:Number}(x::AbstractArray{T}, λ::T)
+    for i in eachindex(x)
+        @inbounds x[i] = soft_thresh(x[i], λ)
+    end
+end
+
+#-------------------------------------------------------------------------# L1Penalty
+immutable L1Penalty <: Penalty end
+value{T<:Number}(p::L1Penalty, λ::T, x::T) = λ * abs(x)
+deriv{T<:Number}(p::L1Penalty, λ::T, x::T) = λ * sign(x)
+prox{T<:Number}(p::L1Penalty, λ::T, x::T) = λ * soft_thresh(x, λ)
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,15 @@
+module Tests
 using Penalties
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+
+
+
+@testset "L1Penalty" begin
+    p = L1Penalty()
+    β = randn(10)
+    @test value(p, .1, β) ≈ .1 * sumabs(β)
+    @test deriv(p, .1, β[1]) ≈ .1 * sign(β[1])
+    @test grad(p, .1, β) ≈ .1 * sign.(β)
+end
+end
