ee297cbc7841bb7a9b5c404b8f789eaa1419d0f2
Josh Day <emailjoshday@gmail.com>
2016-08-20 14:11:44 -0400

    basic structure

--- src/Penalties.jl
+++ src/Penalties.jl
@@ -7,41 +7,59 @@ export
     L2Penalty,
     ElasticNetPenalty
 
+typealias AA{T} AbstractArray{T}
+
 #------------------------------------------------------------------# abstract methods
-function value{T <: Number}(p::Penalty, x::AbstractArray{T})
+function value{T <: Number}(p::Penalty, x::AA{T})
     result = zero(T)
     for xi in x
         result += value(p, xi)
     end
     result
 end
+function value{T <: Number}(p::Penalty, x::AA{T}, factor::AA{T})
+    @assert size(x) == size(factor)
+    result = zero(T)
+    for i in eachindex(x)
+        @inbounds result += factor[i] * value(p, x[i])
+    end
+    result
+end
 
-function grad!{T<:Number}(dest::AbstractArray{T}, p::Penalty, x::AbstractArray{T})
+
+function grad!{T<:Number}(dest::AA{T}, p::Penalty, x::AA{T})
     @assert size(dest) == size(x)
     for i in eachindex(dest)
         @inbounds dest[i] = deriv(p, x[i])
     end
     dest
 end
-function grad{T<:Number}(p::Penalty, x::AbstractArray{T})
-    dest = zeros(x)
-    grad!(dest, p, x)
+function grad!{T<:Number}(dest::AA{T}, p::Penalty, x::AA{T}, factor::AA{T})
+    @assert size(dest) == size(x) == size(factor)
+    for i in eachindex(dest)
+        @inbounds dest[i] = factor[i] * deriv(p, x[i])
+    end
+    dest
 end
 
-function prox!{T<:Number}(p::Penalty, x::AbstractArray{T})
+
+function prox!{T<:Number}(p::Penalty, x::AA{T})
     for i in eachindex(x)
         @inbounds x[i] = prox(p, x[i])
     end
     x
 end
-
-
-soft_thresh{T<:Number}(x::T, λ::T) = sign(x) * max(zero(x), abs(x) - λ)
-function soft_thresh!{T<:Number}(x::AbstractArray{T}, λ::T)
+function prox!{T<:Number}(p::Penalty, x::AA{T}, factor::AA{T})
+    @assert size(x) == size(factor)
     for i in eachindex(x)
-        @inbounds x[i] = soft_thresh(x[i], λ)
+        @inbounds x[i] = prox(p, x[i], factor[i] * p.λ)
     end
+    x
 end
+
+
+soft_thresh{T<:Number}(x::T, λ::T) = sign(x) * max(zero(T), abs(x) - λ)
+
 
 
 #-------------------------------------------------------------------------# L1Penalty
@@ -50,10 +68,36 @@ type L1Penalty{T <: Number} <: Penalty
     λ::T
 end
 L1Penalty(λ::Number = 0.1) = L1Penalty(λ)
-value{T<:Number}(p::L1Penalty, x::T) = p.λ * abs(x)
-deriv{T<:Number}(p::L1Penalty, x::T) = p.λ * sign(x)
-prox{T<:Number}(p::L1Penalty, x::T) = soft_thresh(x, p.λ)
+value{T<:Number}(p::L1Penalty{T}, x::T) = p.λ * abs(x)
+deriv{T<:Number}(p::L1Penalty{T}, x::T) = p.λ * sign(x)
+prox{T<:Number}(p::L1Penalty{T}, x::T, λ::T = p.λ) = soft_thresh(x, λ)
 
+#-------------------------------------------------------------------------# L2Penalty
+"Squared L2-Norm Penalty: f(x) = vecnorm(x, 2) ^ 2"
+type L2Penalty{T <: Number} <: Penalty
+    λ::T
+end
+L2Penalty(λ::Number = 0.1) = L2Penalty(λ)
+value{T<:Number}(p::L2Penalty{T}, x::T) = p.λ * T(.5) * x * x # FIXME?
+deriv{T<:Number}(p::L2Penalty{T}, x::T) = p.λ * x
+prox{T<:Number}(p::L2Penalty{T}, x::T, λ::T = p.λ) = x / (one(T) + λ)
+
+#-----------------------------------------------------------------------# ENetPenalty
+"Weighted average of L1Penalty and L2Penalty"
+type ENetPenalty{T <: Number} <: Penalty
+    λ::T
+    α::T
+end
+ENetPenalty(λ::Number = 0.1, α::Number = 0.5) = ENetPenalty(λ, α)
+function value{T<:Number}(p::ENetPenalty{T}, x::T)
+    p.λ * (p.α * abs(x) + (one(T) - p.α) * T(.5) * x * x) # FIXME?
+end
+function deriv{T<:Number}(p::ENetPenalty{T}, x::T)
+    p.λ * (p.α * sign(x) + (one(T) - p.α) * x)
+end
+function prox{T<:Number}(p::ENetPenalty{T}, x::T, λ::T = p.λ)
+    soft_thresh(x / (one(T) + (one(T) - p.α) * λ), p.α * λ)
+end
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,14 +2,49 @@ module Tests
 using LearnBase, Penalties
 using Base.Test
 
-
-
-
 @testset "L1Penalty" begin
     p = L1Penalty(.1)
     β = randn(10)
+    storage = zeros(10)
     @test value(p, β) ≈ .1 * sumabs(β)
     @test deriv(p, β[1]) ≈ .1 * sign(β[1])
-    @test grad(p, β) ≈ .1 * sign.(β)
+    grad!(storage, p, β)
+    @test storage ≈ .1 * sign.(β)
+    @test prox(p, β[1]) ≈ sign(β[1]) * max(0., abs(β[1]) - .1)
 end
+
+@testset "L2Penalty" begin
+    p = L2Penalty(.1)
+    β = randn(10)
+    storage = zeros(10)
+    @test value(p, β) ≈ .5 * .1 * sumabs2(β)
+    @test deriv(p, β[1]) ≈ .1 * β[1]
+    grad!(storage, p, β)
+    @test storage ≈ .1 * β
+    @test prox(p, β[1]) ≈ β[1] / (1.0 + 0.1)
+end
+
+@testset "Abstract methods" begin
+    p = L1Penalty(.1)
+    β = randn(10)
+    βcopy = deepcopy(β)
+    factor = rand(10)
+    storage = zeros(10)
+
+    @test value(p, β) ≈ p.λ * sumabs(β)
+    @test value(p, β, factor) ≈ p.λ * sum(factor .* abs(β))
+
+    grad!(storage, p, β)
+    @test storage ≈ p.λ * sign(β)
+    grad!(storage, p, β, factor)
+    @test storage ≈ p.λ * sign(β) .* factor
+
+    prox!(p, β)
+    @test β ≈ Penalties.soft_thresh.(βcopy, p.λ)
+    β = deepcopy(βcopy)
+    prox!(p, β, factor)
+    @test β ≈ [Penalties.soft_thresh(βcopy[i], p.λ * factor[i]) for i in 1:10]
+
+end
+
 end
