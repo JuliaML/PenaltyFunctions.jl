7074ee5c18a63343ee38b18ff340d2e57d47a1f0
Josh Day <emailjoshday@gmail.com>
2016-10-14 13:02:38 -0400

    elementwise cleanup.  Replace x with θ

--- src/elementwise.jl
+++ src/elementwise.jl
@@ -1,55 +1,55 @@
 # Penalties that can be evaluated elementwise
 
 #------------------------------------------------------------------# abstract methods
-function value{T <: Number}(p::ElementwisePenalty, x::AA{T})
+function value{T <: Number}(p::ElementwisePenalty, θ::AA{T})
     result = zero(T)
-    for xi in x
-        result += value(p, xi)
+    for θi in θ
+        result += value(p, θi)
     end
     result
 end
-value{T <: Number}(p::ElementwisePenalty, x::AA{T}, s::T) = value(p, x) * s
-function value{T <: Number}(p::ElementwisePenalty, x::AA{T}, s::AA{T})
-    @assert size(x) == size(s)
+value{T <: Number}(p::ElementwisePenalty, θ::AA{T}, s::T) = value(p, θ) * s
+function value{T <: Number}(p::ElementwisePenalty, θ::AA{T}, s::AA{T})
+    @assert size(θ) == size(s)
     result = zero(T)
-    for i in eachindex(x)
-        @inbounds result += value(p, x[i], s[i])
+    for i in eachindex(θ)
+        @inbounds result += value(p, θ[i], s[i])
     end
     result
 end
 
 
-function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, x::AA{T})
-    @assert size(dest) == size(x)
+function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, θ::AA{T})
+    @assert size(dest) == size(θ)
     for i in eachindex(dest)
-        @inbounds dest[i] = deriv(p, x[i])
+        @inbounds dest[i] = deriv(p, θ[i])
     end
     dest
 end
-function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, x::AA{T}, s::T)
-    @assert size(dest) == size(x)
+function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, θ::AA{T}, s::T)
+    @assert size(dest) == size(θ)
     for i in eachindex(dest)
-        @inbounds dest[i] = deriv(p, x[i], s)
+        @inbounds dest[i] = deriv(p, θ[i], s)
     end
     dest
 end
-function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, x::AA{T}, s::AA{T})
-    @assert size(dest) == size(x) == size(s)
+function grad!{T<:Number}(dest::AA{T}, p::ElementwisePenalty, θ::AA{T}, s::AA{T})
+    @assert size(dest) == size(θ) == size(s)
     for i in eachindex(dest)
-        @inbounds dest[i] = deriv(p, x[i], s[i])
+        @inbounds dest[i] = deriv(p, θ[i], s[i])
     end
     dest
 end
 
 function addgrad!{T<:Number}(∇::AA{T}, penalty::ElementwisePenalty, θ::AA{T})
-    @inbounds for (i, θᵢ) in zip(eachindex(∇), θ)
-        ∇[i] += deriv(penalty, θᵢ)
+    @inbounds for (i, θi) in zip(eachindex(∇), θ)
+        ∇[i] += deriv(penalty, θi)
     end
     ∇
 end
 function addgrad!{T<:Number}(∇::AA{T}, penalty::ElementwisePenalty, θ::AA{T}, s::T)
-    @inbounds for (i, θᵢ) in zip(eachindex(∇), θ)
-        ∇[i] += deriv(penalty, θᵢ, s)
+    @inbounds for (i, θi) in zip(eachindex(∇), θ)
+        ∇[i] += deriv(penalty, θi, s)
     end
     ∇
 end
@@ -62,44 +62,44 @@ function addgrad!{T<:Number}(∇::AA{T}, penalty::ElementwisePenalty, θ::AA{T},
 end
 
 
-function prox!{T<:Number}(p::ElementwisePenalty, x::AA{T})
-    for i in eachindex(x)
-        @inbounds x[i] = prox(p, x[i])
+function prox!{T<:Number}(p::ElementwisePenalty, θ::AA{T})
+    for i in eachindex(θ)
+        @inbounds θ[i] = prox(p, θ[i])
     end
-    x
+    θ
 end
-function prox!{T<:Number}(p::ElementwisePenalty, x::AA{T}, s::T)
-    for i in eachindex(x)
-        @inbounds x[i] = prox(p, x[i], s)
+function prox!{T<:Number}(p::ElementwisePenalty, θ::AA{T}, s::T)
+    for i in eachindex(θ)
+        @inbounds θ[i] = prox(p, θ[i], s)
     end
-    x
+    θ
 end
-function prox!{T<:Number}(p::ElementwisePenalty, x::AA{T}, s::AA{T})
-    @assert size(x) == size(s)
-    for i in eachindex(x)
-        @inbounds x[i] = prox(p, x[i], s[i])
+function prox!{T<:Number}(p::ElementwisePenalty, θ::AA{T}, s::AA{T})
+    @assert size(θ) == size(s)
+    for i in eachindex(θ)
+        @inbounds θ[i] = prox(p, θ[i], s[i])
     end
-    x
+    θ
 end
 
 
-value{T<:Number}(p::ElementwisePenalty, x::T, s::T) = s * value(p, x)
-deriv{T<:Number}(p::ElementwisePenalty, x::T, s::T) = s * deriv(p, x)
-prox(p::ElementwisePenalty, x::Number) = _prox(p, x, p.λ)
-prox{T<:Number}(p::ElementwisePenalty, x::T, s::T) = _prox(p, x, p.λ * s)
+value{T<:Number}(p::ElementwisePenalty, θ::T, s::T) = s * value(p, θ)
+deriv{T<:Number}(p::ElementwisePenalty, θ::T, s::T) = s * deriv(p, θ)
+prox(p::ElementwisePenalty, θ::Number) = _prox(p, θ, p.λ)
+prox{T<:Number}(p::ElementwisePenalty, θ::T, s::T) = _prox(p, θ, p.λ * s)
 
 
 #-------------------------------------------------------------------------# NoPenalty
-"No Penalty: f(x) = 0"
+"No Penalty: f(θ) = 0"
 type NoPenalty <: ElementwisePenalty end
-value(p::NoPenalty, x::Number) = zero(x)
-deriv(p::NoPenalty, x::Number) = zero(x)
-prox{T<:Number}(p::NoPenalty, x::T) = x
-prox{T<:Number}(p::NoPenalty, x::T, s::T) = x
+value(p::NoPenalty, θi::Number) = zero(θi)
+deriv(p::NoPenalty, θi::Number) = zero(θi)
+prox{T<:Number}(p::NoPenalty, θi::T) = θi
+prox{T<:Number}(p::NoPenalty, θi::T, s::T) = θi
 
 
 #-------------------------------------------------------------------------# L1Penalty
-"L1-Norm Penalty: f(x) = vecnorm(x, 1)"
+"L1-Norm Penalty: f(θ) = vecnorm(θ, 1)"
 type L1Penalty{T <: Number} <: ElementwisePenalty
     λ::T
 end
@@ -107,13 +107,13 @@ function L1Penalty(λ::Number = 0.1)
     @assert λ >= zero(λ)
     L1Penalty(λ)
 end
-value{T<:Number}(p::L1Penalty{T}, x::T) = p.λ * abs(x)
-deriv{T<:Number}(p::L1Penalty{T}, x::T) = p.λ * sign(x)
-_prox{T<:Number}(p::L1Penalty{T}, x::T, λ::T) = soft_thresh(x, λ)
+value{T<:Number}(p::L1Penalty{T}, θi::T) = p.λ * abs(θi)
+deriv{T<:Number}(p::L1Penalty{T}, θi::T) = p.λ * sign(θi)
+_prox{T<:Number}(p::L1Penalty{T}, θi::T, λ::T) = soft_thresh(θi, λ)
 
 
 #-------------------------------------------------------------------------# L2Penalty
-"Squared L2-Norm Penalty: f(x) = vecnorm(x, 2) ^ 2"
+"Squared L2-Norm Penalty: f(θ) = 0.5 * vecnorm(θ, 2) ^ 2"
 type L2Penalty{T <: Number} <: ElementwisePenalty
     λ::T
 end
@@ -121,9 +121,9 @@ function L2Penalty(λ::Number = 0.1)
     @assert λ >= zero(λ)
     L2Penalty(λ)
 end
-value{T<:Number}(p::L2Penalty{T}, x::T) = p.λ * T(.5) * x * x
-deriv{T<:Number}(p::L2Penalty{T}, x::T) = p.λ * x
-_prox{T<:Number}(p::L2Penalty{T}, x::T, λ::T) = x / (one(T) + λ)
+value{T<:Number}(p::L2Penalty{T}, θi::T) = p.λ * T(.5) * θi * θi
+deriv{T<:Number}(p::L2Penalty{T}, θi::T) = p.λ * θi
+_prox{T<:Number}(p::L2Penalty{T}, θi::T, λ::T) = θi / (one(T) + λ)
 
 #-----------------------------------------------------------------# ElasticNetPenalty
 # λ₁ = λ * α, λ₂ = λ * (1 - α)
@@ -137,15 +137,15 @@ function ElasticNetPenalty(λ::Number = 0.1, α::Number = 0.5)
     @assert zero(λ) <= α <= one(λ)
     ElasticNetPenalty(λ, α)
 end
-function value{T<:Number}(p::ElasticNetPenalty{T}, x::T)
-    p.λ * (p.α * abs(x) + (one(T) - p.α) * T(.5) * x * x)
+function value{T<:Number}(p::ElasticNetPenalty{T}, θi::T)
+    p.λ * (p.α * abs(θi) + (one(T) - p.α) * T(.5) * θi * θi)
 end
-function deriv{T<:Number}(p::ElasticNetPenalty{T}, x::T)
-    p.λ * (p.α * sign(x) + (one(T) - p.α) * x)
+function deriv{T<:Number}(p::ElasticNetPenalty{T}, θi::T)
+    p.λ * (p.α * sign(θi) + (one(T) - p.α) * θi)
 end
-function _prox{T<:Number}(p::ElasticNetPenalty{T}, x::T, λ::T)
+function _prox{T<:Number}(p::ElasticNetPenalty{T}, θi::T, λ::T)
     αλ = p.α * λ
-    soft_thresh(x, αλ) / (one(T) + λ - αλ)
+    soft_thresh(θi, αλ) / (one(T) + λ - αλ)
 end
 
 
@@ -163,36 +163,36 @@ function SCADPenalty(λ::Number = 0.1, a::Number = 3.7)
     @assert a > T(2)
     SCADPenalty(λ, a)
 end
-function value{T <: Number}(p::SCADPenalty{T}, x::T)
-    abx = abs(x)
+function value{T <: Number}(p::SCADPenalty{T}, θi::T)
+    absθ = abs(θi)
     a, λ = p.a, p.λ
-    if abx < λ
-        return λ * abx
-    elseif abx <= λ * a
-        return -T(0.5) * (abx ^ 2 - T(2) * a * λ * abx + λ ^ 2) / (a - one(T))
+    if absθ < λ
+        return λ * absθ
+    elseif absθ <= λ * a
+        return -T(0.5) * (absθ ^ 2 - T(2) * a * λ * absθ + λ ^ 2) / (a - one(T))
     else
         return T(0.5) * (a + one(T)) * λ * λ
     end
 end
-function deriv{T <: Number}(p::SCADPenalty{T}, x::T)
-    abx = abs(x)
+function deriv{T <: Number}(p::SCADPenalty{T}, θi::T)
+    absθ = abs(θi)
     a, λ = p.a, p.λ
-    if abx < λ
-        return λ * sign(x)
-    elseif abx <= λ * a
-        return -(abx - a * λ * sign(x)) / (a - one(T))
+    if absθ < λ
+        return λ * sign(θi)
+    elseif absθ <= λ * a
+        return -(absθ - a * λ * sign(θi)) / (a - one(T))
     else
         return zero(T)
     end
 end
-function _prox{T <: Number}(p::SCADPenalty{T}, x::T, λ::T)
+function _prox{T <: Number}(p::SCADPenalty{T}, θi::T, λ::T)
     a = p.a
-    abx = abs(x)
-    if abx < T(2) * λ
-        return soft_thresh(x, λ)
-    elseif abx <= λ * a
-        return (x - λ * sign(x) * a / (a - one(T))) / (one(T) - one(T) / (a - one(T)))
+    absθ = abs(θi)
+    if absθ < T(2) * λ
+        return soft_thresh(θi, λ)
+    elseif absθ <= λ * a
+        return (θi - λ * sign(θi) * a / (a - one(T))) / (one(T) - one(T) / (a - one(T)))
     else
-        return x
+        return θi
     end
 end
