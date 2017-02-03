94b8b2a48c5d56dc16c317dbd6d7dae8428cdd77
Josh Day <emailjoshday@gmail.com>
2017-02-03 16:10:50 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 module Tests
 using LearnBase, PenaltyFunctions, Base.Test
-
+P = PenaltyFunctions
 
 #---------------------------------------------------------------------------# Begin Tests
 
 @testset "Common" begin
-    @test PenaltyFunctions.soft_thresh(1.0, 0.5) == 0.5
-    @test PenaltyFunctions.soft_thresh!(ones(5), .5) == .5 * ones(5)
-    @test PenaltyFunctions.name(L1Penalty()) == "L1Penalty"
+    @test P.soft_thresh(1.0, 0.5) == 0.5
+    @test P.soft_thresh!(ones(5), .5) == .5 * ones(5)
+    @test P.name(L1Penalty()) == "L1Penalty"
 end
 
 
 @testset "ElementPenalty" begin
     function test_element_penalty(p::Penalty, θ::Number, s::Number, v1, v2, v3)
-        @testset "$(PenaltyFunctions.name(p))" begin
+        @testset "$(P.name(p))" begin
             @test value(p, θ)   ≈ v1
             @test deriv(p, θ)   ≈ v2
             @test prox(p, θ, s) ≈ v3
@@ -23,16 +23,23 @@ end
     θ, s = rand(), rand()
 
     test_element_penalty(NoPenalty(), θ, s, 0.0, 0.0, θ)
-
     test_element_penalty(L1Penalty(), θ, s, abs(θ), sign(θ), max(0.0, θ - sign(θ) * s))
-
     test_element_penalty(L2Penalty(), θ, s, .5 * θ^2, θ, θ / (1 + s))
-
     test_element_penalty(ElasticNetPenalty(.4), θ, s,
         .4 * value(L1Penalty(), θ) + .6 * value(L2Penalty(), θ),
         .4 * deriv(L1Penalty(), θ) + .6 * deriv(L2Penalty(), θ),
         prox(L2Penalty(), prox(L1Penalty(), θ, .4s), .6s)
     )
+
+    @testset "SCADPenalty" begin
+        p = SCADPenalty(3.8)
+
+        @test value(p, .1, .2) ≈ .02
+        @test value(p, .2, .1) ≈ -.5 * (.2^2 - .2^2 * 3.8 + .01) / (2.8)
+        @test value(p, 10., .1) ≈ .5 * 4.8 * .01
+
+        # TODO: same thing as value for deriv and prox
+    end
 
     @testset "ElementPenalty methods" begin
         p = L1Penalty()
@@ -64,6 +71,13 @@ end
             ∇ = rand(10)
             ∇2 = copy(∇)
             addgrad!(∇, p, θ, s); @test ∇ ≈ ∇2 + s .* sign.(θ)
+        end
+        @testset "prox" begin
+            p = L1Penalty()
+            θ2 = copy(θ)
+            prox!(p, θ, s[1]); @test θ ≈ P.soft_thresh.(θ2, s[1])
+            θ = copy(θ2)
+            prox!(p, θ, s); @test θ ≈ P.soft_thresh.(θ2, s)
         end
     end
 end
