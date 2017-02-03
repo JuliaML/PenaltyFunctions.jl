8a5a470caac102fd02290e1f3d42d7c6d93a49e2
Josh Day <emailjoshday@gmail.com>
2017-02-03 12:06:17 -0500

    testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -31,6 +31,19 @@ using LearnBase, PenaltyFunctions, Base.Test
         buffer2 = copy(buffer)
         addgrad!(buffer, p, θ); @test buffer == buffer2
     end
+    @testset "L1Penalty" begin
+        p = L1Penalty()
+        θ = rand(10)
+        θ2 = copy(θ)
+        s = rand(10)
+        buffer = rand(10)
+
+        @test value(p, θ[1])        ≈ abs(θ[1])
+        @test value(p, θ[1], s[1])  ≈ s[1] * abs(θ[1])
+        @test value(p, θ)           ≈ sum(abs, θ)
+        @test value(p, θ, s[1])     ≈ s[1] * sum(abs, θ)
+        @test value(p, θ, s)        ≈ sum(s .* abs.(θ))
+    end
 end
 
 end
